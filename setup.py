f130b6a14940f5e7d49d812354a869a65dfdc0ac
fjornelas <jornela1@g.ucla.edu>
2024-07-06 05:36:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,38 @@
 """A module which goes through the setup of files"""
 from setuptools import setup, find_packages
 
+
+def parse_meta(path_to_meta):
+    """
+    A function to parse through meta.py file
+
+    path_to_meta: string
+        metadata path.
+
+    returns metadata from meta.py file
+
+    """
+    with open(path_to_meta) as f:
+        meta = {}
+        for line in f.readlines():
+            if line.startswith("__version__"):
+                meta["__version__"] = line.split('"')[1]
+    return meta
+
+
+meta = parse_meta("hvsrprocpy/meta.py")
+
 with open("README.md", encoding="utf8") as f:
     long_description = f.read()
 
 setup(
     name='hvsrprocpy',
-    version='0.1.1',
-    description='A package for performing horizontal-to-vertical spectral ratio (HVSR) processing',
+    version=meta['__version__'],
+    description='A python library for performing horizontal-to-vertical spectral ratio (HVSR) processing',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/fjornelas/hvsrprocpy',
-    license='GNU General Public License v3 or later (GPLv3+)',
+    license='GNU General Public License v3',
     author='Francisco Javier Ornelas',
     author_email='jornela1@g.ucla.edu',
     classifiers=[
@@ -25,20 +46,23 @@ setup(
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Physics',
 
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Natural Language :: English',
+
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+
+        'Topic :: Software Development :: Version Control :: Git',
     ],
-    keywords='HVSR, FAS, Time Series, microtremors  ',
+    keywords='HVSR, FAS, Time Series, microtremors',
     packages=find_packages(),
     python_requires='>=3.8',
-    install_requires=['numpy>=1.20', 'pandas>1.4', 'matplotlib>3.8', 'os', 'obspy>1.3', 'mplcursors>0.4'],
+    install_requires=['numpy>=1.20', 'pandas', 'matplotlib', 'obspy', 'scipy',
+                      'parzenpy'],
     project_urls={
         'Bug Reports': 'https://github.com/fjornelas/hvsrprocpy/issues',
         'Source': 'https://github.com/fjornelas/hvsrprocpy/',
+        'Docs': 'https://fjornelas.github.io/hvsrprocpy/'
     },
 )
-
