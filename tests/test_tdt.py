03ca43a09d8ae12a08f6bc69577dee448d9fdd51
fjornelas <jornela1@g.ucla.edu>
2024-07-07 09:26:26 -0700

    Update test_tdt.py

--- tests/test_tdt.py
+++ tests/test_tdt.py
@@ -7,7 +7,7 @@ from hvsrprocpy.tdt import detrend_ts, tukey_window, apply_filter, apply_normali
 
 
 def test_detrend_ts():
-    ts = np.linspace(1, 100, 100)
+    ts = np.linspace(1, 100, 200)
 
     detrended = detrend_ts(ts, detrend_type=1)
     win = tukey(len(ts), alpha=((10 + 10) / 200))  # Default Tukey window with 10% taper on each side
@@ -27,13 +27,13 @@ def test_detrend_ts():
 
 
 def test_tukey_window():
-    ts = np.linspace(1, 100, 100)
+    ts = np.linspace(1, 100, 200)
     windowed = tukey_window(ts, t_front=10, t_end=10)
     assert len(windowed) == len(ts)
 
 
 def test_apply_filter():
-    ts = np.random.randn(100)
+    ts = np.random.randn(200)
     filtered, res = apply_filter(ts, ts_dt=0.005, fc=0.042, npole=-5, is_causal=False, order_zero_padding=2)
     assert len(filtered) == len(ts)
     assert 'flt_ts' in res
@@ -43,22 +43,22 @@ def test_apply_filter():
 
 
 def test_apply_normalization():
-    ts = np.linspace(1, 100, 100)
+    ts = np.linspace(1, 100, 200)
     normalized = apply_normalization(ts)
     assert np.isclose(np.linalg.norm(normalized), 1)
 
 
 def test_sta_lta_calc():
-    ts = np.random.randn(100)
+    ts = np.random.randn(200)
     ratio = sta_lta_calc(ts, short_term=5, long_term=20, moving_term=1)
     assert len(ratio) == (len(ts) - 5) // 1 + 1
 
 
 def test_split_into_windows():
-    ts = np.random.randn(100)
-    windows = split_into_windows(ts, dt=1, win_width=10, overlapping=5)
-    assert windows.shape[0] == (len(ts) // (10 - 0))
-    assert windows.shape[1] == (10 - 5)
+    ts = np.random.randn(200)
+    windows = split_into_windows(ts, dt=1, win_width=20, overlapping=0)
+    assert windows.shape[0] == (len(ts) // (20 - 0))
+    assert windows.shape[1] == (20 - 0)
 
 
 if __name__ == "__main__":
