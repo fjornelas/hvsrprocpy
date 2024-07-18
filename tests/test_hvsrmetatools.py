78ac0f94edf6d9602e0466f72534a19b5d0df7be
Javier Ornelas <jornela1@g.ucla.edu>
2024-07-18 15:03:03 -0700

    Update test_hvsrmetatools.py

--- tests/test_hvsrmetatools.py
+++ tests/test_hvsrmetatools.py
@@ -11,7 +11,7 @@ def setup_test_data():
     yield test_data_dir
 
 def test_process_xml_file(setup_test_data):
-    xml_file = 'NX.USC3_20240530T184335Z.xml'  # Located in the test_data directory
+    xml_file = 'NX.USC3_20240530T184335Z.xml'  
     xml_file_path = os.path.join(setup_test_data, xml_file)
     sta, cha = HvsrMetaTools.process_xml_file(xml_file_path)
     assert sta is not None
@@ -35,6 +35,21 @@ def test_process_hvsr_metadata(setup_test_data):
     df = HvsrMetaTools.process_hvsr_metadata(field_crew, user, weather, ground_type, sensor_ground_coupling,
                                             monochromatic, building, transients, data_type,
                                             sensor_corner_frequency, gain, comments, setup_test_data)
+
+    assert len(df) == 1
+
+def test_process_hvsr_metadata_for_a_single_site(setup_test_data):
+    
+    field_crew = None
+    user = 'john doe'
+    weather = None
+    ground_type = None
+    sensor_ground_coupling = None
+    xml_file = 'NX.USC3_20240530T184335Z.xml' 
+    xml_file_path = os.path.join(setup_test_data, xml_file)
+    inventory_path=xml_file_path
+
+    df = HvsrMetaTools.process_hvsr_metadata_single_site(field_crew, user, weather, ground_type, sensor_ground_coupling, inventory_path=inventory_path)
 
     assert len(df) == 1
 
