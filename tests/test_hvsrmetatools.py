75967f7719682e6e0274f9d7ed5d60537d7bdd29
Javier Ornelas <jornela1@g.ucla.edu>
2024-07-18 13:24:24 -0700

    Update test_hvsrmetatools.py

--- tests/test_hvsrmetatools.py
+++ tests/test_hvsrmetatools.py
@@ -17,31 +17,30 @@ def test_process_xml_file(setup_test_data):
     assert sta is not None
     assert cha is not None
 
-def test_add_to_dataframe():
-    df = pd.DataFrame(columns=['name', 'longitude', 'latitude', 'elevation', 'start_date', 'end_date',
-                               'start_time', 'end_time', 'field_crew', 'seismic_recorder',
-                               'recorder_serial_number', 'gain', 'sensor_manufacturer', 'sensor',
-                               'sensor_name', 'sensor_serial_number', 'sensor_corner_frequency',
-                               'weather', 'ground_type', 'monochromatic_noise_source',
-                               'sensor_ground_coupling', 'building', 'transients', 'azimuth',
-                               'user', 'comments', 'data_type', 'mass_position_w', 'mass_position_v',
-                               'mass_position_u', 'mag_dec'])
+def test_process_hvsr_metadata(setup_test_data):
+    
+    field_crew = None
+    user = 'john doe'
+    weather = None
+    ground_type = None
+    sensor_ground_coupling = None
+    monochromatic = None
+    building = None
+    transients = None
+    data_type = 0
+    sensor_corner_frequency = 0.0083
+    gain = 1
+    comments = None
 
+    df = HvsrMetaTools.process_hvsr_metadata(field_crew, user, weather, ground_type, sensor_ground_coupling,
+                                            monochromatic, building, transients, data_type,
+                                            sensor_corner_frequency, gain, comments, setup_test_data)
 
-    sta = {'description': 'Test Station', 'site': None, 'code': 'TS1'}
-    cha = {'longitude': 0.0, 'latitude': 0.0, 'elevation': 100.0, 'start_date': '2024-07-18', 'end_date': '2024-07-18',
-           'data_logger': None, 'sensor': None}
-    df = HvsrMetaTools.add_to_dataframe(df, sta, cha, 'John Doe','DOE', 'Weather', 'Ground', 'Buried', 'Yes', 'No','No',
-                                        0, 0.0083, 1, 'No comments')
-    assert len(df) == 1
-
-def test_process_hvsr_metadata_single_site():
-    df = HvsrMetaTools.process_hvsr_metadata_single_site('John Doe','Doe','Weather', 'Ground', 'Buried')
     assert len(df) == 1
 
 def test_create_mean_curves_csv(setup_test_data):
     add_sim_path = 'additional_path'
-    df = HvsrMetaTools.create_mean_curves_csv(setup_test_data, 'test_mean_curves.csv', add_sim_path)
+    df = HvsrMetaTools.create_mean_curves_csv(setup_test_data, 'Test_hvsr_mean.csv', add_sim_path)
     assert df is not None
     assert isinstance(df, pd.DataFrame)
 
